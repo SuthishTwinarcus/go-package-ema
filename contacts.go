6ef624ea69f4d11ec9172cd09fffcd2164c1a314
naveentwinarcus <naveen@twinarcus.com>
2025-03-06 19:22:10 +0530

    Updated package with new features/fixes

--- contacts.go
+++ contacts.go
@@ -17,23 +17,29 @@ type Contact struct {
 		Zipcode     string `json:"zipcode"`
 	} `json:"data"`
 }
+type Status struct {
+	Status string `json:"status"`
+}
+type ContactTag struct {
+	TagId string `json:"tag_id"`
+}
 
 func (c *Client) CreateContact(listID string, contact Contact) ([]byte, error) {
 	endpoint := fmt.Sprintf("contacts/%s/create", listID)
 	return c.request("POST", endpoint, contact)
 }
 
-func (c *Client) UnsubscribeContact(listID string, contactId string, contact Contact) ([]byte, error) {
+func (c *Client) UnsubscribeContact(listID string, contactId string, status Status) ([]byte, error) {
 	endpoint := fmt.Sprintf("contacts/%s/status/%s", listID, contactId)
-	return c.request("POST", endpoint, contact)
+	return c.request("POST", endpoint, status)
 }
 
-func (c *Client) AddTag(contactId string, contact Contact) ([]byte, error) {
+func (c *Client) AddTag(contactId string, contactTag ContactTag) ([]byte, error) {
 	endpoint := fmt.Sprintf("contacts/tags/%s/add", contactId)
-	return c.request("POST", endpoint, contact)
+	return c.request("POST", endpoint, contactTag)
 }
 
-func (c *Client) RemoveContactTag(contactId string, contact Contact) ([]byte, error) {
+func (c *Client) RemoveContactTag(contactId string, contactTag ContactTag) ([]byte, error) {
 	endpoint := fmt.Sprintf("contacts/tags/%s/remove", contactId)
-	return c.request("POST", endpoint, contact)
+	return c.request("POST", endpoint, contactTag)
 }
