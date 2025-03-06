619a10d5fb7b8bc263970880ce30f7d28d3076c8
naveentwinarcus <naveen@twinarcus.com>
2025-03-06 17:37:21 +0530

    Updated package with new features/fixes

--- contacts.go
+++ contacts.go
@@ -3,26 +3,37 @@ package yonoma
 import "fmt"
 
 type Contact struct {
-	Email string `json:"email"`
-	Name  string `json:"name,omitempty"`
+	Email  string `json:"contact_email"`
+	Status string `json:"sub_status"`
+	Data   struct {
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Phone     string `json:"mobile_number"`
+		Gender    string `json:"gender"`
+		Address   string `json:"address"`
+		City      string `json:"city"`
+		State     string `json:"state"`
+		Country   string `json:"country"`
+		Zipcode   string `json:"zipcode"`
+	} `json:"data"`
 }
 
-func (c *Client) CreateContact(listID string, contactData Contact) ([]byte, error) {
+func (c *Client) CreateContact(listID string, contact Contact) ([]byte, error) {
 	endpoint := fmt.Sprintf("contacts/%s/create", listID)
-	return c.request("POST", endpoint, contactData)
+	return c.request("POST", endpoint, contact)
 }
 
-func (c *Client) UnsubscribeContact(listID string, contactId string, contactData map[string]interface{}) ([]byte, error) {
+func (c *Client) UnsubscribeContact(listID string, contactId string, contact Contact) ([]byte, error) {
 	endpoint := fmt.Sprintf("contacts/%s/status/%s", listID, contactId)
-	return c.request("POST", endpoint, contactData)
+	return c.request("POST", endpoint, contact)
 }
 
-func (c *Client) AddTag(contactId string, contactData map[string]interface{}) ([]byte, error) {
+func (c *Client) AddTag(contactId string, contact Contact) ([]byte, error) {
 	endpoint := fmt.Sprintf("contacts/tags/%s/add", contactId)
-	return c.request("POST", endpoint, contactData)
+	return c.request("POST", endpoint, contact)
 }
 
-func (c *Client) RemoveContactTag(contactId string, contactData map[string]interface{}) ([]byte, error) {
+func (c *Client) RemoveContactTag(contactId string, contact Contact) ([]byte, error) {
 	endpoint := fmt.Sprintf("contacts/tags/%s/remove", contactId)
-	return c.request("POST", endpoint, contactData)
+	return c.request("POST", endpoint, contact)
 }
